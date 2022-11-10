e609f7b43aa9deaf9109ca05b6ee9847478538c1
Samuel Seng <djsamseng@gmail.com>
2022-11-10 12:13:40 -0500

    Add scripts for blocking and unblocking some outgoing traffic

---
+++ UbuntuInstall/unblock_internet.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+users=`cat /etc/passwd | cut -d: -f1`
+# groups $USER
+users=("samuel" "adm" "cdrom" "dip" "plugdev" "lpadmin" "lxd" "sambashare")
+for user in "${users[@]}";
+do
+	sudo iptables -D OUTPUT -m owner --gid-owner $user -j DROP
+	sudo ip6tables -D OUTPUT -m owner --gid-owner $user -j DROP
+done
+
